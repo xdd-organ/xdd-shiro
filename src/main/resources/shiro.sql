00fe848d9d2e995d7b479db356f01835159a3c3f
qq767403510 <767403510@qq.com>
2017-08-20 21:17:42 +0800

    数据库表

---
+++ src/main/resources/shiro.sql
@@ -0,0 +1,113 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.6.22-log : Database - shiro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shiro` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `shiro`;
+
+/*Table structure for table `abc` */
+
+DROP TABLE IF EXISTS `abc`;
+
+CREATE TABLE `abc` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `value` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `abc` */
+
+insert  into `abc`(`id`,`name`,`value`) values (1,'safsd','fsdaf'),(2,'sdgd','gdfgd');
+
+/*Table structure for table `permission` */
+
+DROP TABLE IF EXISTS `permission`;
+
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `code` varchar(20) DEFAULT NULL COMMENT '标识',
+  `name` varchar(20) DEFAULT NULL COMMENT '名称',
+  `url` varchar(64) DEFAULT NULL COMMENT 'URL',
+  `type` int(2) DEFAULT NULL COMMENT '类型',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `permission` */
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `code` varchar(20) DEFAULT NULL COMMENT '标识',
+  `name` varchar(20) DEFAULT NULL COMMENT '角色名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `role` */
+
+/*Table structure for table `role_permission` */
+
+DROP TABLE IF EXISTS `role_permission`;
+
+CREATE TABLE `role_permission` (
+  `permission_id` int(11) DEFAULT NULL COMMENT '权限id',
+  `role_id` int(11) DEFAULT NULL COMMENT '角色id',
+  KEY `FK_role_permission` (`permission_id`),
+  KEY `FK2_role_permission` (`role_id`),
+  CONSTRAINT `FK2_role_permission` FOREIGN KEY (`role_id`) REFERENCES `permission` (`id`),
+  CONSTRAINT `FK_role_permission` FOREIGN KEY (`permission_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `role_permission` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(20) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(16) DEFAULT NULL COMMENT '盐',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`salt`) values (1,'admin','admin','123');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `user_id` int(11) DEFAULT NULL COMMENT '关联用户',
+  `role_id` int(11) DEFAULT NULL COMMENT '关联角色',
+  KEY `FK2_user_role` (`user_id`),
+  KEY `FK_user_role` (`role_id`),
+  CONSTRAINT `FK2_user_role` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_user_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`user_id`,`role_id`) values (1,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
